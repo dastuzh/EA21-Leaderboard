96026af5d55289d4cdc5ac3f26cd00b0bc68c16a
imhaozhe <imhaozhe@gmail.com>
2021-05-07 18:24:19 +0200

    delete test

--- js/data.js
+++ js/data.js
@@ -6,14 +6,6 @@ const students=[
         "submission time": "07/05/2021 14:06:16",
         "update time": "07/05/2021 17:03:42",
         "timestamp": "1620399822"
-    },
-    {
-        "repository": "EA21-test",
-        "preprocessing time (ms)": "340165",
-        "execution time (ms)": "272930",
-        "submission time": "07/05/2021 16:06:07",
-        "update time": "07/05/2021 18:07:50",
-        "timestamp": "1620403670"
     }
 ]
         
