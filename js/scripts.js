dfa77073c9e749606d85d6aaf794e245e37475fa
DAST <67323857+dastuzh@users.noreply.github.com>
2021-05-07 16:27:14 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,28 @@
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
 
+const students=[
+    {
+        "repository": "EA21-17",
+        "preprocessing time (ms)": "439594",
+        "execution time (ms)": "304076",
+        "submission time": "07/05/2021 09:43:00",
+        "update time": "07/05/2021 16:06:16",
+        "timestamp": "1620396376"
+    },
+    {
+        "repository": "EA21-test",
+        "preprocessing time (ms)": "340165",
+        "execution time (ms)": "272930",
+        "submission time": "07/05/2021 14:16:26",
+        "update time": "07/05/2021 16:17:11",
+        "timestamp": "1620397031"
+    }
+]
+        
+        const last_update_time=07/05/2021 16:23:21
+        
+
 var app = new Vue({
     el: '#app',
     data: {
