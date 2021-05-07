2e824d6b01f1d6236500e40a5e9e9163855c1679
Haozhe Zhang <zhang@dast-cpu01.ifi.uzh.ch>
2021-05-07 16:06:18 +0200

    update data

--- js/data.js
+++ js/data.js
@@ -1,10 +1,10 @@
 const students=[
     {
-        "repository": "EA21-test",
-        "preprocessing time (ms)": "340165",
-        "execution time (ms)": "272930",
-        "submission time": "07/05/2021 11:38:25",
-        "update time": "07/05/2021 15:31:16",
-        "timestamp": "1620394276"
+        "repository": "EA21-17",
+        "preprocessing time (ms)": "439594",
+        "execution time (ms)": "304076",
+        "submission time": "07/05/2021 09:43:00",
+        "update time": "07/05/2021 16:06:16",
+        "timestamp": "1620396376"
     }
 ]
